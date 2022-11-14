9dcf0b601a5ac95d17d7b91469b22bbda44e845d
Nikolai <luker1998@yandex.ru>
2022-11-14 22:57:30 +0300

    Задача 6

---
+++ Example015/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №6
+
+// Напишите программу, которая на вход
+// принимает число и выдаёт, является ли число чётным
+// (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 0) Console.WriteLine("Число равно нулю");
+else if(number%2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
