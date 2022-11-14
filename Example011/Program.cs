38a3e21d40d33607fb1d9c055fa286747a86b7ab
Nikolai <luker1998@yandex.ru>
2022-11-14 22:18:45 +0300

    Task 5

---
+++ Example011/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a = -number - 1;
+
+while (a < number)
+{
+    a = a + 1;
+    Console.Write($"{a} ");
+}
