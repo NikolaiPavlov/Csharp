7b36fedec35e7712cb01abd196ea14f20d714ce6
Nikolai <luker1998@yandex.ru>
2022-11-14 22:26:17 +0300

    task 12

---
+++ Example012/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(99 < number)
+{
+    if(number < 1000)
+    {
+        Console.WriteLine(number = number % 10);
+    }
+    else Console.WriteLine("Это не трехзначное число");
+}
+else Console.WriteLine("Это не трехзначное число");
