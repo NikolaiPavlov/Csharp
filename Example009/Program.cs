1904f74c3b79899a1a2612bdb06f5829b77f85b1
Nikolai <luker1998@yandex.ru>
2022-11-14 21:37:46 +0300

    New commit Example009

---
+++ Example009/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2 * number2)
+{
+    Console.WriteLine($"Да, число {number1} является квадратом числа {number2}");
+}
+else Console.WriteLine($"Нет, число {number1} неявляется квадратом числа {number2}");
