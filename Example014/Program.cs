c11b59c669237bb1d8134dbca01076c961f6f92c
Nikolai <luker1998@yandex.ru>
2022-11-14 23:09:59 +0300

    Задача 4 Update

--- Example014/Program.cs
+++ Example014/Program.cs
@@ -12,19 +12,10 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > number2)
-{
-    if (number1 > number3)
-    {
-        Console.WriteLine($"max = {number1}");
-    }
-    else Console.WriteLine($"max = {number3}");
-}
-else
-{
-    if (number2 > number3)
-    {
-        Console.WriteLine($"max = {number2}");
-    }
-    else Console.WriteLine($"max = {number3}");
-}
+int max = number1;
+
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
