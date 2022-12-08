b954405e6e169e4a2085f3b3f02a2f1eba6aaf4f
Nikolai <luker1998@yandex.ru>
2022-12-08 20:48:55 +0300

    Task 24

---
+++ Example039/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 24: Напишите программу, 
+//которая принимает на вход число (А) 
+//и выдаёт сумму чисел от 1 до А.
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма от 1 до числа {number} = {FindSumToLimit(number)}");
+
+int FindSumToLimit(int limit)
+{
+    int sum = 0;
+
+    for (int i = 0; i < limit + 1; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
