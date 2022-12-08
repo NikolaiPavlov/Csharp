e01cb57e29383ad0c22f4bc6e7b5023e5f162590
Nikolai <luker1998@yandex.ru>
2022-12-08 20:56:31 +0300

    Задача 25

---
+++ Example040/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 25
+//Напишите цикл, который принимает на вход 
+//два числа (A и B) и возводит число A 
+// натуральную степень B.
+
+Console.Clear();
+
+Console.WriteLine("Введите число A: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(exponentiation(number1, number2));
+
+int exponentiation(int a, int b)
+{
+    int exp = 1;
+
+    for (int i = 0; i < b; i++)
+    {
+        exp = exp * a;
+    }
+
+    return exp;
+}
