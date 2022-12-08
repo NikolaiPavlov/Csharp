a75fa2f49e2bcf42883be5ded70900c432d0ceab
Nikolai <luker1998@yandex.ru>
2022-12-08 22:08:46 +0300

    Task 45

---
+++ Example045/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 30: Напишите программу, которая выводит 
+//массив из 8 элементов, заполненный нулями и 
+//единицами в случайном порядке.
+
+Console.Clear();
+
+int[] FillArray(int n)
+{
+    Random rnd = new Random();
+    int[] array = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+    return array;
+}
+
+int[] arr = FillArray(8);
+
+Console.WriteLine(string.Join(" ", arr));
