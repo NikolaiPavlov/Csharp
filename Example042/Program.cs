1b2af493087136449eb1848a2c58e848c6ea1d68
Nikolai <luker1998@yandex.ru>
2022-12-08 21:28:42 +0300

    Задача 27

---
+++ Example042/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 27
+//Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindLength(int a)
+{
+    int length = 0;
+
+    while (a > 0)
+    {
+        a = a / 10;
+        length++;
+    }
+    return length;
+}
+
+int FindSum(int a)
+{
+    int sum = 0;
+    for(int i = 0; i < FindLength(number); i++)
+    {
+       int b = a % 10;
+        a = a / 10;
+    sum = sum + b; 
+    }
+    return sum;
+}
+
+Console.WriteLine(FindSum(number));
