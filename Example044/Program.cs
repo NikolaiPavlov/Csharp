19084eddadaa3945efda7ae52cb322a2d7e16d10
Nikolai <luker1998@yandex.ru>
2022-12-08 22:18:06 +0300

    Залача 29

---
+++ Example044/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 29
+//Напишите программу, которая задаёт массив 
+//из N элементов и выводит их на экран.
+
+Console.Clear();
+
+Console.WriteLine("Введите размерность массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] FillArray(int n)
+{
+    Random rnd = new Random();
+    int[] array = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+int[] arr = FillArray(number);
+
+Console.WriteLine(string.Join(" ", arr));
