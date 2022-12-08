69507f3cb4fa0d63981eb1027f1c4df5219284a9
Nikolai <luker1998@yandex.ru>
2022-12-08 21:20:31 +0300

    Task 26

---
+++ Example041/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 26: Напишите программу, которая 
+//принимает на вход число и выдаёт количество 
+//цифр в числе.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindLength(int a)
+{
+    int length = 0;
+
+    while (a > 0)
+    {
+        a = a / 10;
+        length++;
+    }
+    return length;
+}
+
+Console.WriteLine(FindLength(number));
