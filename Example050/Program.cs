b48013ac78ca73f35d7df0e93b178e8cf7146661
Nikolai <luker1998@yandex.ru>
2022-12-09 22:02:04 +0300

    Task 35

---
+++ Example050/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 35
+//Задайте одномерный массив из 123 случайных чисел. 
+//Найдите количество элементов массива, значения которых 
+//лежат в отрезке [10,99]. 
+
+Console.Clear();
+
+int[] array = FillArray();
+Console.WriteLine($"Получившийся массив: [{string.Join(", ", array)}]");
+
+Console.WriteLine($"Количество четных чисел в массиве: {FindValueInArray(array)}");
+
+int[] FillArray()
+{
+    Random random = new Random();
+
+    int[] resultArray = new int[123];
+
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = random.Next(0, 1000);
+    }
+
+    return resultArray;
+}
+
+int FindValueInArray(int[] inputArray)
+{
+    int count = 0;
+
+     for(int i = 0; i < inputArray.Length; i++)
+    {
+        if(inputArray[i] >= 10 && inputArray[i] <= 99)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
