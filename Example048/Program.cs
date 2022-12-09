292d53351f0383e7c1952b9b8f98506a6b6d5fa9
Nikolai <luker1998@yandex.ru>
2022-12-09 21:45:37 +0300

    Task 33

---
+++ Example048/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задача 33
+//Задайте массив. Напишите программу, которая определяет, 
+//присутствует ли заданное число в массиве.
+
+Console.Clear();
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимально допустимое значение в массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимально допустимое значение в массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число для поиска в массиве");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(size, min, max);
+Console.WriteLine($"Получившийся массив: [{string.Join(", ", array)}]");
+
+int[] FillArray(int sizeArray, int minValue, int maxValue)
+{
+    Random random = new Random();
+
+    int[] resultArray = new int[sizeArray];
+
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = random.Next(minValue, maxValue + 1);
+    }
+
+    return resultArray;
+}
+
+bool FindNumberInArray(int[] inputArray, int num)
+{
+    for(int i = 0; i < inputArray.Length; i++)
+    {
+        if(inputArray[i] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+if(FindNumberInArray(array, number) == true)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
+
