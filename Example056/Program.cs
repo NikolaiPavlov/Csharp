f761ac46398fab50e7dd100748aa78a526563894
Nikolai <luker1998@yandex.ru>
2022-12-10 13:21:20 +0300

    Лекция 4

---
+++ Example056/Program.cs
@@ -0,0 +1,10 @@
+﻿double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for(int i = 0; i<20; i++)
+{
+    Console.WriteLine($"{i!} = {Factorial(i)}");
+}
