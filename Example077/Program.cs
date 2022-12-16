76320fc82c73cef243952145d70b4b82b4cdba96
Nikolai <luker1998@yandex.ru>
2022-12-16 21:12:08 +0300

    Задача 54

---
+++ Example077/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задача 54
+//Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки 
+//двумерного массива.
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] StreamlineRows(int[,] inputArray)
+{
+   for (int i = 0; i < inputArray.GetLength(0); i++)
+   {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inputArray.GetLength(1) - 1; k++)
+            {
+                if (inputArray[i, k] < inputArray[i, k + 1])
+                {
+                int temp = inputArray[i, k + 1];
+                inputArray[i, k + 1] = inputArray[i, k];
+                inputArray[i, k] = temp;
+                }
+            }
+        }
+    }
+    return inputArray;
+}
+
+int[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+
+StreamlineRows(array);
+
+PrintArray(array);
+
