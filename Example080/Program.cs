0392c0cd79ff9dedc704212d1a7e880b87a29df6
Nikolai <luker1998@yandex.ru>
2022-12-20 19:44:23 +0300

    Find Factorial by recursion

---
+++ Example080/Program.cs
@@ -0,0 +1,16 @@
+﻿//Нахождение факториала с помощью рекурсии
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int FindFactorial(int n)
+{
+    if (n == 1 || n == 0) return 1;
+
+    return n * FindFactorial(n-1);
+}
+
+Console.WriteLine($"Факториал числа {value} = {FindFactorial(value)}");
+
