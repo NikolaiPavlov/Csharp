6d21ba2375094488d7d1abfccfd22b12ac3cc889
Nikolai <luker1998@yandex.ru>
2022-12-20 20:17:03 +0300

    Задача 64

---
+++ Example081/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 64
+//Задайте значения M и N. Напишите программу, 
+//которая выведет все чётные натуральные числа в промежутке 
+//от M до N с помощью рекурсии.
+
+Console.Clear();
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void FindEvenNumbers(int min, int max)
+{
+    if (min > max) return;
+    if (min % 2 == 0 && min <= max) Console.Write($"{min} ");
+    FindEvenNumbers(min + 1, max);
+}
+
+FindEvenNumbers(m, n);
