e2ed50d14e65bf54dd803b46a3766c53a503d67e
Nikolai <luker1998@yandex.ru>
2022-12-22 20:46:03 +0300

    Task 65

---
+++ Example085/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 65: Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке 
+//от M до N с помощью рекурсии
+
+Console.Clear();
+Console.Write("Введите число M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"M = {m}, N = {n} ->");
+PrintValue(m, n);
+
+void PrintValue(int val, int max)
+{
+    if (val > max) return;
+    Console.Write($" {val}");
+    PrintValue(val + 1, max);
+}
