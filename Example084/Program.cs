dbffad6c9b551aa5a98fe01ecde80b07c190d562
Nikolai <luker1998@yandex.ru>
2022-12-22 20:44:28 +0300

    Task 63

---
+++ Example084/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке 
+//от 1 до N с помощью рекурсии
+
+Console.Clear();
+
+Console.Write("Введите число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintValue(1, n);
+
+void PrintValue(int m, int max)
+{
+    if (m > max) return;
+    Console.Write($" {m}");
+    PrintValue(m + 1, max);
+}
